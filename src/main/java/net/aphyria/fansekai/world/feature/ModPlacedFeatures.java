cdb8000da29a107c48b2bbe369450e7a08962cd2
Aphyria <132872632+Aphyria@users.noreply.github.com>
2023-06-26 02:07:09 +0200

    + geode generation capability + set Act 0 mineables to be mineable
    • almost done with cluster gen jsons, might adjust where they spawn in code later
    
    ~ transparency on glass needs work
    ~ note to future aphy: add the goddamn loot tables and smelt recipes bish

--- src/main/java/net/aphyria/fansekai/world/feature/ModPlacedFeatures.java
+++ src/main/java/net/aphyria/fansekai/world/feature/ModPlacedFeatures.java
@@ -22,7 +22,7 @@ public class ModPlacedFeatures {
     public static final RegistryObject<PlacedFeature> HELIOTROPE_ORE_PLACED = PLACED_FEATURES.register("heliotrope_ore_placed",
             ()-> new PlacedFeature(ModConfigFeatures.HELIOTROPE_ORE.getHolder().get(),
                     rareOrePlacement(2, HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(20), VerticalAnchor.belowTop(20)))));
-    public static final RegistryObject<PlacedFeature> TURQUISE_ORE_PLACED = PLACED_FEATURES.register("turquoise_ore_placed",
+    public static final RegistryObject<PlacedFeature> TURQUIOSE_ORE_PLACED = PLACED_FEATURES.register("turquoise_ore_placed",
             ()-> new PlacedFeature(ModConfigFeatures.TURQUOISE_ORE.getHolder().get(),
                     commonOrePlacement(2, HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(0), VerticalAnchor.absolute(20)))));
     public static final RegistryObject<PlacedFeature> PSEUDOCROCIDOLITE_ORE_PLACED = PLACED_FEATURES.register("pseudocrocidolite_ore_placed",
@@ -31,6 +31,25 @@ public class ModPlacedFeatures {
     public static final RegistryObject<PlacedFeature> RHODONITE_ORE_PLACED = PLACED_FEATURES.register("rhodonite_ore_placed",
             ()-> new PlacedFeature(ModConfigFeatures.RHODONITE_ORE.getHolder().get(),
                     commonOrePlacement(2, HeightRangePlacement.triangle(VerticalAnchor.aboveBottom(0), VerticalAnchor.absolute(20)))));
+
+    public static final RegistryObject<PlacedFeature> JAIDO_GEODE_PLACED = PLACED_FEATURES.register("jaido_geode_placed",
+            () -> new PlacedFeature(ModConfigFeatures.JAIDO_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(30), InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.absolute(15), VerticalAnchor.absolute(100)))));
+    public static final RegistryObject<PlacedFeature> HOWLITE_GEODE_PLACED = PLACED_FEATURES.register("howlite_geode_placed",
+            () -> new PlacedFeature(ModConfigFeatures.HOWLITE_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(30), InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(15), VerticalAnchor.absolute(20)))));
+    public static final RegistryObject<PlacedFeature> HELIOTROPE_GEODE_PLACED = PLACED_FEATURES.register("heliotrope_geode_placed",
+            () -> new PlacedFeature(ModConfigFeatures.HELIOTROPE_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(75), InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(5), VerticalAnchor.belowTop(5)))));
+    public static final RegistryObject<PlacedFeature> TURQUOISE_GEODE_PLACED = PLACED_FEATURES.register("turquoise_geode_placed",
+            () -> new PlacedFeature(ModConfigFeatures.TURQUOISE_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(50), InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(15), VerticalAnchor.absolute(40)))));
+    public static final RegistryObject<PlacedFeature> PSEUDOCROCIDOLITE_GEODE_PLACED = PLACED_FEATURES.register("pseudocrocidolite_geode_placed",
+            () -> new PlacedFeature(ModConfigFeatures.PSEUDOCROCIDOLITE_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(50), InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(15), VerticalAnchor.absolute(40)))));
+    public static final RegistryObject<PlacedFeature> RHODONITE_GEODE_PLACED = PLACED_FEATURES.register("rhodonite_geode_placed",
+            () -> new PlacedFeature(ModConfigFeatures.RHODONITE_GEODE.getHolder().get(), List.of(
+                    RarityFilter.onAverageOnceEvery(50), InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.aboveBottom(15), VerticalAnchor.absolute(40)))));
 
 
 
